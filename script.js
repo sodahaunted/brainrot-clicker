60a2b52e3c5a934d6e12fcb53c551185896e2927
nic <nicole.nakim@gmail.com>
2026-09-18 22:27:43 -0400

    feat poem upgrade, parsedNumbers

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 let number = document.querySelector('.value')
+let parsedNumber = parseFloat(number.innerHTML)
 
 function incrementValue() {
-    number.innerHTML = parseFloat(number.innerHTML) + 1
+    parsedNumber += 1
+    number.innerHTML = parsedNumber
 }
+
+const box = document.getElementById("sayori");
+
+sayori.addEventListener("click", function() {
+  box.animate(
+    [
+      { transform: "translateY(0px)" },
+      { transform: "translateY(-10px)" }
+    ],
+    {
+      duration: 200,
+      iterations: 2,
+      direction: "alternate",
+      easing: "ease-in-out"
+    })
+  });
+
+  let clickerCost = document.querySelector('.clicker-cost')
+  let parsedClickerCost = parseFloat(clickerCost.innerHTML)
+
+  function buyClick() {
+    if(parsedNumber >= parsedClickerCost) {
+      parsedNumber -= parsedClickerCost
+      number.innerHTML = parsedNumber
+    }
+  }
